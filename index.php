8a4634bdbfcdfddaf0592e0492c93ce09340984c
Hsnlarifin <95298277+Hsnlarifin@users.noreply.github.com>
2022-12-13 03:10:16 +0800

    connection to database

--- index.php
+++ index.php
@@ -11,7 +11,14 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    V-Rental
+  </a>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary"> 
   <a class="navbar-brand" href="#">V-Rental</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
