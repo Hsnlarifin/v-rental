1535d0227f9cc3bf616801d61cdff01917d9777d
Hsnlarifin <95298277+Hsnlarifin@users.noreply.github.com>
2022-12-13 02:10:48 +0800

    added css, images folder
    ds

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<1-- >test< -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> V-Rental </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bottsrap/4.5.2/css.bootstrap.min.css">
+    <link rel = "stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+<script src = 
+
+</body>
+</html>
