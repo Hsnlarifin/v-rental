9eda93c59d96afb2b2cce97749a9f67f15d9e995
Hsnlarifin <95298277+Hsnlarifin@users.noreply.github.com>
2022-12-19 03:05:44 +0800

    mybookings edited

--- my-booking.php
+++ my-booking.php
@@ -11,7 +11,7 @@ else{
 <html lang="en">
 <head>
 
-<title>Car Rental Portal - My Booking</title>
+<title>V-Rental - My Reservation</title>
 <!--Bootstrap -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 <!--Custome Style -->
@@ -40,7 +40,7 @@ else{
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link rel="shortcut icon" href="images/tab_icon.png">
 <!-- Google-Font-->
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -53,24 +53,25 @@ else{
 <body>
 
 <!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
+
 <!-- /Switcher -->  
         
 <!--Header-->
 <?php include('includes/header.php');?>
 <!--Page Header-->
+
 <!-- /Header --> 
 
 <!--Page Header-->
 <section class="page-header profile_page">
   <div class="container">
-    <div class="page-header_wrap">
+    <div class="">
       <div class="page-heading">
-        <h1>My Booking</h1>
+        <h1>My Reservation</h1>
       </div>
       <ul class="coustom-breadcrumb">
         <li><a href="#">Home</a></li>
-        <li>My Booking</li>
+        <li>My Reservation</li>
       </ul>
     </div>
   </div>
@@ -109,7 +110,7 @@ foreach($results as $result)
    
       <div class="col-md-8 col-sm-8">
         <div class="profile_wrap">
-          <h5 class="uppercase underline">My Booikngs </h5>
+          <h5 class="uppercase underline">RESERVATION</h5>
           <div class="my_vehicles_list">
             <ul class="vehicle_listing">
 <?php 
