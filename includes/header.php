07921bbf48640ec850d4c51f5ce8c75743163a6d
Hsnlarifin <95298277+Hsnlarifin@users.noreply.github.com>
2023-01-17 13:18:29 +0800

    profile page

--- includes/header.php
+++ includes/header.php
@@ -66,8 +66,8 @@ foreach($results as $result)
             <li><a href="profile.php">Profile Settings</a></li>
               <li><a href="update-password.php">Update Password</a></li>
             <li><a href="my-booking.php">My Booking</a></li>
-            <li><a href="post-testimonial.php">Post a Testimonial</a></li>
-          <li><a href="my-testimonials.php">My Testimonial</a></li>
+            <!-- <li><a href="post-testimonial.php">Post a Testimonial</a></li>
+         <li><a href="my-testimonials.php">My Testimonial</a></li> -->
             <li><a href="logout.php">Sign Out</a></li>
             <?php } ?>
           </ul>
